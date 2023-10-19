1183c0ec079a6d0596651c87b8cef7c071385517
maleksoufi1 <malek.soufi@esprit.tn>
2023-10-19 10:03:59 +0100

    Test

---
+++ DevOps_Project/src/test/java/tn/esprit/devops_project/services/ActivitySectorImplTest.java
@@ -0,0 +1,26 @@
+package tn.esprit.devops_project.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import tn.esprit.devops_project.entities.ActivitySector;
+import tn.esprit.devops_project.services.Iservices.IActivitySector;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class ActivitySectorImplTest {
+    @Autowired
+    IActivitySector iActivitySector;
+
+@Test
+    public void retrieveAllActivitySectors()
+    {
+
+        List<ActivitySector> activitySectors = iActivitySector.retrieveAllActivitySectors();
+        Assertions.assertEquals(0,activitySectors.size());
+    }
+
+}
