4a5c0bd311004902d5e2b0318d2b1626feb1ac2f
Hazem Bayoudh <98537270+BHazem@users.noreply.github.com>
2023-10-20 18:18:00 +0100

    commit test

---
+++ DevOps_Project/src/test/java/tn/esprit/devops_project/services/ProductServiceImplTest.java
@@ -0,0 +1,30 @@
+package tn.esprit.devops_project.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import tn.esprit.devops_project.entities.Product;
+import tn.esprit.devops_project.services.Iservices.IProductService;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest
+class ProductServiceImplTest {
+
+
+
+    @Autowired
+    IProductService iProductService;
+
+
+    @Test
+    public void retreiveAllProduct() {
+        List<Product> listProducts = iProductService.retreiveAllProduct();
+        Assertions.assertEquals(0,listProducts.size());
+    }
+
+}
