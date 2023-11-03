b7ceadd912176177f11f2225fe4a1fe5bc071f92
Hazem Bayoudh <98537270+BHazem@users.noreply.github.com>
2023-11-03 22:08:45 +0100

    commit 28

--- DevOps_Project/src/test/java/tn/esprit/devops_project/services/ProductServiceImplTest.java
+++ DevOps_Project/src/test/java/tn/esprit/devops_project/services/ProductServiceImplTest.java
@@ -5,10 +5,12 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import tn.esprit.devops_project.entities.Product;
 
@@ -28,6 +30,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 
 @SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class ProductServiceImplTest {
 
     List<Product> productList = new ArrayList<Product>() {
