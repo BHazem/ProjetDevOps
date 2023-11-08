2715145e00da167b047d3e9b2dce65f5be74c420
farah kamoun <61378665+farahkm@users.noreply.github.com>
2023-11-08 17:51:34 +0100

    Update SupplierController.javazzz

--- DevOps_Project/src/main/java/tn/esprit/devops_project/controllers/SupplierController.java
+++ DevOps_Project/src/main/java/tn/esprit/devops_project/controllers/SupplierController.java
@@ -13,26 +13,31 @@ import java.util.List;
 public class SupplierController {
 
 	ISupplierService supplierService;
+@CrossOrigin(origins = "http://192.168.176.160:4200", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 
 	@GetMapping("/supplier")
 	public List<Supplier> getSuppliers() {
 		return supplierService.retrieveAllSuppliers();
 	}
+@CrossOrigin(origins = "http://192.168.176.160:4200", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 
 	@GetMapping("/supplier/{supplierId}")
 	public Supplier retrieveSupplier(@PathVariable Long supplierId) {
 		return supplierService.retrieveSupplier(supplierId);
 	}
+@CrossOrigin(origins = "http://192.168.176.160:4200", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 
 	@PostMapping("/supplier")
 	public Supplier addSupplier(@RequestBody Supplier supplier) {
 		return supplierService.addSupplier(supplier);
 	}
+@CrossOrigin(origins = "http://192.168.176.160:4200", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 
 	@DeleteMapping("/supplier/{supplierId}")
 	public void removeFournisseur(@PathVariable Long supplierId) {
 		supplierService.deleteSupplier(supplierId);
 	}
+@CrossOrigin(origins = "http://192.168.176.160:4200", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 
 	@PutMapping("/supplier")
 	public Supplier modifyFournisseur(@RequestBody Supplier supplier) {
