d7a2e6af7b037240cbe0929211b444a621f1634e
soulaima537 <61362270+soulaima537@users.noreply.github.com>
2023-11-09 02:33:28 +0100

    commit/teststock

--- DevOps_Project/src/test/java/tn/esprit/devops_project/services/StockServiceImplTest.java
+++ DevOps_Project/src/test/java/tn/esprit/devops_project/services/StockServiceImplTest.java
@@ -13,11 +13,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.stereotype.Service;
 import tn.esprit.devops_project.entities.Stock;
-
+import tn.esprit.devops_project.dto.StockDto;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
+import java.util.Collections;
+import java.util.List;
 import tn.esprit.devops_project.repositories.StockRepository;
 
 import static org.assertj.core.api.Assertions.as;
@@ -25,12 +26,65 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.any;
 
 @ExtendWith(MockitoExtension.class)
 @SpringBootTest
 
 class StockServiceImplTest {
+
+    @Mock
+    private StockRepository stockRepository;
+
+    @InjectMocks
+    private StockServiceImpl stockService;
+
+    @Test
+    void addStock_ShouldReturnNewStock() {
+        // Arrange
+        StockDto stockDto = new StockDto(); // Assume StockDto is properly set up
+        Stock stock = new Stock(); // Assume Stock is properly set up
+        when(stockRepository.save(any(Stock.class))).thenReturn(stock);
+
+        // Act
+        Stock result = stockService.addStock(stockDto);
+
+        // Assert
+        assertNotNull(result);
+        verify(stockRepository).save(any(Stock.class));
+    }
+
+    @Test
+    void retrieveStock_ShouldReturnStock() {
+        // Arrange
+        Long id = 1L;
+        Stock stock = new Stock(); // Assume Stock is properly set up
+        when(stockRepository.findByIdStock(id)).thenReturn(stock);
+
+        // Act
+        Stock result = stockService.retrieveStock(id);
+
+        // Assert
+        assertNotNull(result);
+        assertEquals(stock, result);
+        verify(stockRepository).findByIdStock(id);
+    }
+
+    @Test
+    void retrieveAllStock_ShouldReturnStockList() {
+        // Arrange
+        when(stockRepository.findAll()).thenReturn(Collections.singletonList(new Stock()));
+
+        // Act
+        List<Stock> result = stockService.retrieveAllStock();
+
+        // Assert
+        assertFalse(result.isEmpty());
+        verify(stockRepository).findAll();
+    }
+
 /*
     @InjectMocks
     private StockServiceImpl stockService;
