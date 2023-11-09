930544158687c51ebc1e4575e750f240305bcab8
soulaima537 <61362270+soulaima537@users.noreply.github.com>
2023-11-09 10:46:15 +0100

    commit/testBkeya

---
+++ DevOps_Project/src/test/java/tn/esprit/devops_project/services/InvoiceServiceImplTest.java
@@ -0,0 +1,145 @@
+package tn.esprit.devops_project.services;
+
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import tn.esprit.devops_project.entities.Invoice;
+
+import tn.esprit.devops_project.repositories.InvoiceRepository;
+import tn.esprit.devops_project.repositories.OperatorRepository;
+import tn.esprit.devops_project.repositories.SupplierRepository;
+import tn.esprit.devops_project.repositories.InvoiceDetailRepository;
+
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+
+class InvoiceServiceImplTest {
+
+    @Mock
+    private InvoiceRepository invoiceRepository;
+
+    @Mock
+    private OperatorRepository operatorRepository;
+
+    @Mock
+    private InvoiceDetailRepository invoiceDetailRepository;
+
+    @Mock
+    private SupplierRepository supplierRepository;
+
+    @InjectMocks
+    private InvoiceServiceImpl invoiceService;
+
+    @BeforeEach
+    void setUp() {
+        // Any setup before each test can go here
+    }
+
+    @Test
+    void retrieveAllInvoices() {
+        // Arrange
+        when(invoiceRepository.findAll()).thenReturn(Collections.singletonList(new Invoice()));
+
+        // Act
+        List<Invoice> result = invoiceRepository.findAll();
+
+        // Assert
+        assertFalse(result.isEmpty());
+        verify(invoiceRepository).findAll();
+    }
+
+    /* @Test
+     void cancelInvoice_ShouldCancelTheInvoice() {
+         // Arrange
+         Long invoiceId = 1L;
+         Invoice invoice = new Invoice();
+         when(invoiceRepository.findById(invoiceId)).thenReturn(Optional.of(invoice));
+         doNothing().when(invoiceRepository).save(any(Invoice.class));
+
+         // Act
+         invoiceService.cancelInvoice(invoiceId);
+
+         // Assert
+         assertTrue(invoice.getArchived());
+         verify(invoiceRepository).save(invoice);
+     }
+ */
+    @Test
+    void retrieveInvoice() {
+        // Arrange
+        Long invoiceId = 1L;
+        Invoice invoice = new Invoice();
+        when(invoiceRepository.findById(invoiceId)).thenReturn(Optional.of(invoice));
+
+        // Act
+        Invoice result = invoiceRepository.findById(invoiceId).orElse(null);
+
+        // Assert
+        assertNotNull(result);
+        verify(invoiceRepository).findById(invoiceId);
+    }
+
+   /* @Test
+      void getInvoicesBySupplier() {
+          // Arrange
+          Long supplierId = 1L;
+          Supplier supplier = mock(Supplier.class);
+          when(supplierRepository.findById(supplierId)).thenReturn(Optional.of(supplier));
+          when(supplier.getInvoices()).thenReturn(null);
+
+          // Act
+          List<Invoice> result = invoiceService.getInvoicesBySupplier(supplierId);
+
+          // Assert
+          assertNotNull(result);
+          verify(supplierRepository).findById(supplierId);
+      }
+*/
+   /* @Test
+    void assignOperatorToInvoice() {
+        // Arrange
+        Long operatorId = 1L;
+        Long invoiceId = 1L;
+        Operator operator = new Operator();
+        Invoice invoice = new Invoice();
+        when(operatorRepository.findById(operatorId).orElse(null));
+        when(invoiceRepository.findById(invoiceId).orElse(null));
+
+        // Act
+        invoiceService.assignOperatorToInvoice(operatorId, invoiceId);
+
+        // Assert
+        assertTrue(operator.getInvoices().contains(invoice));
+        verify(operatorRepository).save(operator);
+    }
+*/
+    @Test
+    void getTotalAmountInvoiceBetweenDates() {
+        // Arrange
+        Date startDate = new Date();
+        Date endDate = new Date();
+        when(invoiceRepository.getTotalAmountInvoiceBetweenDates(startDate, endDate)).thenReturn(100.0f);
+
+        // Act
+        float totalAmount = invoiceRepository.getTotalAmountInvoiceBetweenDates(startDate, endDate);
+
+        // Assert
+        assertEquals(100.0f, totalAmount);
+        verify(invoiceRepository).getTotalAmountInvoiceBetweenDates(startDate, endDate);
+    }
+
+    // Additional test cases...
+}
---
+++ DevOps_Project/src/test/java/tn/esprit/devops_project/services/OperatorServiceImplTest.java
@@ -0,0 +1,110 @@
+package tn.esprit.devops_project.services;
+
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import tn.esprit.devops_project.dto.OperatorDto;
+import tn.esprit.devops_project.entities.Operator;
+import tn.esprit.devops_project.repositories.OperatorRepository;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+
+ class OperatorServiceImplTest {
+
+    @Mock
+    private OperatorRepository operatorRepository;
+
+    @InjectMocks
+    private OperatorServiceImpl operatorService;
+
+    @BeforeEach
+    void setUp() {
+        // Any setup before each test can go here
+    }
+
+    @Test
+    void retrieveAllOperators_ShouldReturnOperatorList() {
+        // Arrange
+        when(operatorRepository.findAll()).thenReturn(Collections.singletonList(new Operator()));
+
+        // Act
+        List<Operator> result = operatorService.retrieveAllOperators();
+
+        // Assert
+        assertFalse(result.isEmpty());
+        verify(operatorRepository).findAll();
+    }
+
+    @Test
+    void addOperator_ShouldReturnNewOperator() {
+        // Arrange
+        OperatorDto operatorDto = new OperatorDto();
+        Operator operator = new Operator();
+        when(operatorRepository.save(any(Operator.class))).thenReturn(operator);
+
+        // Act
+        Operator result = operatorService.addOperator(operatorDto);
+
+        // Assert
+        assertNotNull(result);
+        verify(operatorRepository).save(any(Operator.class));
+    }
+
+    @Test
+    void deleteOperator_ShouldInvokeRepositoryDelete() {
+        // Arrange
+        Long operatorId = 1L;
+        doNothing().when(operatorRepository).deleteById(anyLong());
+
+        // Act
+        operatorService.deleteOperator(operatorId);
+
+        // Assert
+        verify(operatorRepository).deleteById(operatorId);
+    }
+
+    @Test
+    void updateOperator_ShouldReturnUpdatedOperator() {
+        // Arrange
+        OperatorDto operatorDto = new OperatorDto();
+        Operator operator = new Operator();
+        when(operatorRepository.save(any(Operator.class))).thenReturn(operator);
+
+        // Act
+        Operator result = operatorService.updateOperator(operatorDto);
+
+        // Assert
+        assertNotNull(result);
+        verify(operatorRepository).save(any(Operator.class));
+    }
+
+    @Test
+    void retrieveOperator_ShouldReturnOperator() {
+        // Arrange
+        Long operatorId = 1L;
+        Operator operator = new Operator();
+        when(operatorRepository.findById(operatorId)).thenReturn(Optional.of(operator));
+
+        // Act
+        Operator result = operatorService.retrieveOperator(operatorId);
+
+        // Assert
+        assertNotNull(result);
+        verify(operatorRepository).findById(operatorId);
+    }
+
+    // Additional test cases...
+}
